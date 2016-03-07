aefb20102b902737a401c2ece05c3621921aad04
Wenyn-Wang <wenynwang@outlook.com>
2016-03-07 23:56:31 +0800

    save all files

--- RocketFund/__init__.py
+++ RocketFund/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import sqlalchemy 
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 app = Flask(__name__)
 manager = Manager(app)
 
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
